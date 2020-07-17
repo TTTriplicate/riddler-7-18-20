66cb17b161e573012fdf89eef350c4ac3e844460
TTTriplicate <tttriplicate@gmail.com>
2020-07-17 19:42:51 -0400

    using built-in factorial works much better

--- battingAverage.py
+++ battingAverage.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
+import math
+
 def shortSeason(realAvg):
-
-
+    '''
+    60 games, 4 at-bats each
+    .35 chance of a hit each at-bat
+    looking for hits >= (.4 * (60 * 4) )
+    '''
+    n = float(60 * 4)
+    X = n * .4
+    p = avg
+    q = (1 - avg)
+    print(math.factorial(n))
+    #part 1
+    result = (math.factorial(n) / (math.factorial(n - X) * math.factorial(X)))
     return result
 
 def normalSeason(realAvg):
@@ -13,4 +25,4 @@ def normalSeason(realAvg):
 avg = .350
 
 print(shortSeason(avg))
-print(normalSeason(avg))
+#print(normalSeason(avg))
