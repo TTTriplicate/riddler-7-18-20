8adff6e65e72c7b76f17350d9c6d1a48e77ae52f
TTTriplicate <tttriplicate@gmail.com>
2020-07-17 18:48:50 -0400

    added file for the extra part

---
+++ consecutiveHits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def shortSeason(battingAvg):
+
+    return result
+
+def regularSeason(battingAvg):
+    
+    return result
+
+avg = .350
+
+print(shortSeason(avg))
+print(regularSeason(avg))
