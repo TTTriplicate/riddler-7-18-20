2fae09d7c97b083db85a88d21f5b702ad903a6bd
TTTriplicate <tttriplicate@gmail.com>
2020-07-17 18:41:54 -0400

    getting started

---
+++ battingAverage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def shortSeason(realAvg):
+
+
+    return result
+
+def normalSeason(realAvg):
+
+
+    return result
+
+avg = .350
+
+print(shortSeason(avg))
+print(normalSeason(avg))
