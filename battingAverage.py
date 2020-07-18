060206e573a48ae1052f5c7870241d5b0c2103ee
TTTriplicate <tttriplicate@gmail.com>
2020-07-17 21:12:49 -0400

    added main file, small changes for syntax

--- battingAverage.py
+++ battingAverage.py
@@ -5,13 +5,14 @@ import math
 def battingFourHundred(realAvg, seasonLength):
     '''
     60 games, 4 at-bats each
-    .35 chance of a hit each at-bat
+    realAvg chance of a hit each at-bat
     looking for hits >= (.4 * (60 * 4) )
+    math.factorial didn't like X being a float, had to round and cast
     '''
     n = seasonLength * 4
     X = int(round(n * .4, 10))
-    p = avg
-    q = (1 - avg)
+    p = realAvg
+    q = (1 - realAvg)
     #part 1
     result = (math.factorial(n) / (math.factorial(n - X) * math.factorial(X)))
     
@@ -23,7 +24,3 @@ def battingFourHundred(realAvg, seasonLength):
     return result
 
 
-avg = .350
-
-print(battingFourHundred(avg, 60))
-print(battingFourHundred(avg, 162))
