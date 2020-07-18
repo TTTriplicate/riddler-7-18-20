8203f67c050d42448446aaad02d7e28aadb032ed
TTTriplicate <tttriplicate@gmail.com>
2020-07-17 21:17:22 -0400

    added base idea of hitstreak

--- consecutiveHits.py
+++ consecutiveHits.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-def shortSeason(battingAvg):
-
-    return result
-
-def regularSeason(battingAvg):
+def hitStreak(battingAvg, seasonLength):
+    '''
+    take binomial distribution of getting 1 hit or more per game
+    expotentiate to the the (streak) power
+    odds of one in 4 with <battingAvg> chance of success per
+    '''
     
     return result
 
-avg = .350
-
-print(shortSeason(avg))
-print(regularSeason(avg))
