c2e69504c17a3f817550e92a3bbe6c4867384b21
TTTriplicate <tttriplicate@gmail.com>
2020-07-19 15:04:14 -0400

    Cleaning up some debugging, removing deprecated function

--- consecutiveHits.py
+++ consecutiveHits.py
@@ -9,7 +9,7 @@ def markovHitStreak(battingAvg, seasonLength):
     M = np.zeros((streak, streak))
     M[-1, -1] = 1
     perGameHit = perGame(battingAvg)
-    print("per game hit odds =", perGameHit)
+#    print("per game hit odds =", perGameHit)
     M[:-1, 0] = (1 -perGameHit)
     for i in range(streak-1):
         M[i, i+1] = perGameHit
@@ -21,16 +21,4 @@ def perGame(battingAvg):
     result = math.factorial(4)/math.factorial(3)
     result *= battingAvg
     result *= (1 - battingAvg)**3
-    return result
-
-def hitStreak(battingAvg, seasonLength):
-    '''
-    take binomial distribution of getting 1 hit or more per game
-    expotentiate to the the (streak) power
-    odds of one in 4 with <battingAvg> chance of success per
-    '''
-    oddsPerGame = perGame(battingAvg)
-    hitEveryGame = oddsPerGame**seasonLength
-    result = 0
-
     return result
--- seasonStats.py
+++ seasonStats.py
@@ -3,9 +3,24 @@
 import battingAverage
 import consecutiveHits
 
-print(battingAverage.battingFourHundred(.35, 60))
-print(battingAverage.battingFourHundred(.35, 162))
+def both(odds1, odds2):
+    return (odds1 * odds2)
 
-#print(consecutiveHits.hitStreak(.35, 60))
-#print(consecutiveHits.hitStreak(.35, 162))
-print(consecutiveHits.markovHitStreak(.35, 60))
+seasonLength = [60, 162]
+battingAvg = .350 
+
+battingOdds = []
+battingOdds.append(battingAverage.battingFourHundred(battingAvg, seasonLength[0]))
+battingOdds.append(battingAverage.battingFourHundred(battingAvg, seasonLength[1]))
+
+print("Odds of batting a .400 in a short season:", battingAverage.battingFourHundred(battingAvg, seasonLength[0]))
+print("Odds of batting .400 in a regular season:", battingAverage.battingFourHundred(battingAvg, seasonLength[1]))
+
+#print(consecutiveHits.hitStreak(battingAvg, seasonLength[0]))
+#print(consecutiveHits.hitStreak(battingAvg, seasonLength[1]))
+streakChance = []
+streakChance.append(consecutiveHits.markovHitStreak(battingAvg, seasonLength[0]))
+streakChance.append(consecutiveHits.markovHitStreak(battingAvg, seasonLength[1]))
+
+print("Odds of matching or breaking the 56 game streak in a short season:", streakChance[0])
+print("Odds of matching or breaking the 56 game streak in a regular season:", streakChance[1])
