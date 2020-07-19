31ed15fe26eee02e4b1aa60fa59f47146555b745
TTTriplicate <tttriplicate@gmail.com>
2020-07-19 16:07:11 -0400

    Updating readme

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ For this part I used a binomial distribution. I ran a loop, and returned the sum
 For this part, I started by trying to use a binomial distribution, and quickly realizing that was wrong.  From there I did some research and settled on a Markov Chain, using NumPy to model it with a matrix.
 
 Essentially, I made a diagonalized matrix representing the possible outcomes at each stage of the hit-streak, and raised it to the (Number of games) power to represent that many possible games.  I calculated the per-game hit chance using another binomial ditribution.
+
+#Chance of both
+
+Being two seperate events, I just multiplied them together.  I think this is wrong; having the higher batting average would make the streak more likely, for one thing, but not necessarily the inverse.  Coule fold the .400 into the markov chain and use that instead of the chance based on batting .350.
--- seasonStats.py
+++ seasonStats.py
@@ -13,8 +13,8 @@ battingOdds = []
 battingOdds.append(battingAverage.battingFourHundred(battingAvg, seasonLength[0]))
 battingOdds.append(battingAverage.battingFourHundred(battingAvg, seasonLength[1]))
 
-print("Odds of batting a .400 in a short season:", battingAverage.battingFourHundred(battingAvg, seasonLength[0]))
-print("Odds of batting .400 in a regular season:", battingAverage.battingFourHundred(battingAvg, seasonLength[1]))
+print("Odds of batting a .400 in a short season:", battingOdds[0])
+print("Odds of batting .400 in a regular season:", battingOdds[1])
 
 #print(consecutiveHits.hitStreak(battingAvg, seasonLength[0]))
 #print(consecutiveHits.hitStreak(battingAvg, seasonLength[1]))
@@ -24,3 +24,5 @@ streakChance.append(consecutiveHits.markovHitStreak(battingAvg, seasonLength[1])
 
 print("Odds of matching or breaking the 56 game streak in a short season:", streakChance[0])
 print("Odds of matching or breaking the 56 game streak in a regular season:", streakChance[1])
+
+print("Chances of both batting .400 and having a 56 or longer streak in a short season:", battingOdds[0] * streakChance[0])
